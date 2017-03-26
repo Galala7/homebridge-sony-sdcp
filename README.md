a1273df2c463f2e0ec5f07e3a71aef4900137d79
Guy Shapira <git@guyshapira.com>
2017-03-26 23:54:26 +0300

    Initial commit with code!

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # homebridge-sony-sdcp
-Sony SDCP / PJ-Talk projector control accessory plugin for homebridge
+Sony SDCP / PJ Talk projector control accessory plugin for homebridge: https://github.com/nfarina/homebridge
+
+This plugin allows homebridge to control and query the power state of a Sony projector.
+
+Control of the projector is done using Sony's SDCP communication protocol over TCP/IP.
+The protocol allows more control over the projector, like changing Input or Volume, but as
+  far as i understand, Siri can't take advantage of that.
+
+Supported Sony projectors should include:
+* VPL-VW515
+* VPL-VW520
+* VPL-VW528
+* VPL-VW665
+* VPL-VW315
+* VPL-VW320
+* VPL-VW328
+* VPL-VW365
+* VPL-HW65ES
+
+# Installation
+
+1. Install plugin globally: npm install -g homebridge-sony-sdcp
+3. Update your homebridge config file.  Example below:
+
+# Configuration
+
+Add as an accessory by editing the homebridge config.json file.
+
+## Simple Example
+
+```
+"accessories": [
+  {
+    "accessory":      "sony-sdcp-projector",
+    "name":           "Projector",
+    "host":           "192.168.1.19"
+  }
+]
+```
+
+# Credits
+This plugin uses the [sony-sdcp-com](https://github.com/vokkim/sony-sdcp-com) NodeJS library by [vokkim](https://github.com/vokkim).
---
+++ index.js
@@ -0,0 +1,62 @@
+var Service, Characteristic;
+var SdcpClient = require('sony-sdcp-com');
+
+
+PORT = 53484;
+
+
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+
+    homebridge.registerAccessory("homebridge-sony-sdcp", "sony-sdcp-projector", ProjectorAccessory);
+};
+
+function ProjectorAccessory(log, config) {
+    this.log = log;
+    this.config = config;
+    this.name = config["name"];
+    this.lastState = 0;
+    this.myClient = SdcpClient.SdcpClient({address: this.config["ip"], port: PORT});
+
+    this.service = new Service.Switch(this.name);
+    this.service
+        .getCharacteristic(Characteristic.On)
+        .on('get', this.getCurrentState.bind(this))
+        .on('set', this.setCurrentState.bind(this));
+}
+
+
+ProjectorAccessory.prototype.getServices = function() {
+    return [this.service];
+};
+
+ProjectorAccessory.prototype.getCurrentState = function (callback) {
+    this.log("Requested CurrentState: %s", this.lastState);
+    this.myClient.getPower().then (function(status) {
+        if (status === 'COOLING' || status === 'OFF') {
+            this.lastState = 0;
+        } else if (status === 'WARMING' || status === 'ON') {
+            this.lastState = 1;
+        } else {
+            this.log("Error getting stratus: %s", status)
+        }
+        this.log("CurrentState: %s", this.lastState);
+        callback(null,this.lastState);
+    }.bind(this));
+};
+
+ProjectorAccessory.prototype.setCurrentState = function (value, callback) {
+    this.log("Set CurrentState: %s", value);
+    this.log((value ? "Turning On" : "Turning Off"));
+    if (value == 1) {
+        // Turn On
+        this.myClient.setPower(true);
+    }
+    else {
+        // Turn Off
+        this.myClient.setPower(false);
+    }
+    callback(null);
+};
